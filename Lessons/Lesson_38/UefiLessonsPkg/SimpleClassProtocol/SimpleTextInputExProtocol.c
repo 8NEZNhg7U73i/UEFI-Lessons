c95c262afb53b2d7f5c8179f76ac4dca1963005a
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-06-02 03:42:49 +0000

    Update SimpleTextInputExProtocol.c

--- Lessons/Lesson_38/UefiLessonsPkg/SimpleClassProtocol/SimpleTextInputExProtocol.c
+++ Lessons/Lesson_38/UefiLessonsPkg/SimpleClassProtocol/SimpleTextInputExProtocol.c
@@ -7,57 +7,62 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 
-//#include <Protocol/SimpleTextInputEx.h>
+// #include <Protocol/SimpleTextInputEx.h>
 #include <Protocol/SimpleTextInEx.h>
 
-
-EFI_HANDLE  SimpleTextInputExHandle = NULL;
+EFI_HANDLE SimpleTextInputExHandle = NULL;
 
 UINTN mNumber = 0;
 
-//extern EFI_NATIVE_INTERFACE SimpleTextInputEx;
+// extern EFI_NATIVE_INTERFACE SimpleTextInputEx;
 
-//typedef struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL  EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL;
+// typedef struct _EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL  EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL;
 
 EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL SimpleTextInputEx;
 
 EFI_STATUS
 EFIAPI
-SimpleTextInputExProtocolDriverUnload (
-  IN EFI_HANDLE        ImageHandle
-  )
+SimpleTextInputExProtocolDriverUnload(
+    IN EFI_HANDLE ImageHandle)
 {
   Print(L"Bye-bye from SimpleTextInputExProtocol driver, handle=%p\n", SimpleTextInputExHandle);
 
   EFI_STATUS Status = gBS->UninstallMultipleProtocolInterfaces(
-                             SimpleTextInputExHandle,
-                             &gEfiSimpleTextInputExProtocolGuid,
-                             &SimpleTextInputEx,
-                             NULL
-                             );
+      SimpleTextInputExHandle,
+      &gEfiSimpleTextInputExProtocolGuid,
+      &SimpleTextInputEx,
+      NULL);
 
   return Status;
 }
 
 EFI_STATUS
 EFIAPI
-SimpleTextInputExProtocolDriverEntryPoint (
-  IN EFI_HANDLE        ImageHandle,
-  IN EFI_SYSTEM_TABLE  *SystemTable
-  )
+SimpleTextInputExProtocolDriverEntryPoint(
+    IN EFI_HANDLE ImageHandle,
+    IN EFI_SYSTEM_TABLE *SystemTable)
 {
   Print(L"Hello from SimpleTextInputExProtocol driver");
-
-  EFI_STATUS Status = gBS->InstallMultipleProtocolInterfaces(
-                             &SimpleTextInputExHandle,
-                             &gEfiSimpleTextInputExProtocolGuid,
-                             &SimpleTextInputEx,
-                             NULL
-                             );
-  if (!EFI_ERROR(Status))
-    Print(L", handle=%p\n", SimpleTextInputExHandle);
+  EFI_STATUS Status = gBS->LocateProtocol(
+      &gEfiSimpleTextInputExProtocolGuid,
+      NULL,
+      &SimpleTextInputEx);
+  if (Status == EFI_NOT_FOUND)
+  {
+    EFI_STATUS Status = gBS->InstallMultipleProtocolInterfaces(
+        &SimpleTextInputExHandle,
+        &gEfiSimpleTextInputExProtocolGuid,
+        &SimpleTextInputEx,
+        NULL);
+    if (!EFI_ERROR(Status))
+      Print(L", handle=%p\n", SimpleTextInputExHandle);
+    else
+      Print(L"\n", SimpleTextInputExHandle);
+  }
   else
-    Print(L"\n", SimpleTextInputExHandle);
+  {
+    return EFI_ALREADY_STARTED;
+  }
 
   return Status;
 }
